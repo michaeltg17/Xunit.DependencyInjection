af2f1c9bdd08d9e83c95b0bc2c3c6c982051cc58
Michael Trullas Garcia <adoser2@hotmail.es>
2026-08-12 10:29:08 +0200

    will be changed in another PR

--- src/Xunit.DependencyInjection/MethodDataAttribute.cs
+++ src/Xunit.DependencyInjection/MethodDataAttribute.cs
@@ -107,5 +107,5 @@ public sealed class MethodDataAttribute(string methodName, params object?[] para
         return param;
     }
 
-    public override bool SupportsDiscoveryEnumeration() => false;
+    public override bool SupportsDiscoveryEnumeration() => true;
 }
